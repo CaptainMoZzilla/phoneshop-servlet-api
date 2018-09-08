df907ff3314adb70aa9cf9b7dab3f29de0b068a9
Anrix <derbylive145@gmail.com>
2018-09-08 22:09:18 +0300

    Added test methods, minor improvements

--- src/main/java/com/es/phoneshop/web/servlets/ProductListServlet.java
+++ src/main/java/com/es/phoneshop/web/servlets/ProductListServlet.java
@@ -1,10 +1,8 @@
 package com.es.phoneshop.web.servlets;
 
-import com.es.phoneshop.model.*;
-import com.es.phoneshop.model.cart.Cart;
-import com.es.phoneshop.model.cart.CartService;
+import com.es.phoneshop.model.ArrayListProductDao;
+import com.es.phoneshop.model.ProductDao;
 import com.es.phoneshop.web.tools.RequestTools;
-import org.omg.CORBA.Request;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -14,7 +12,6 @@ import java.io.IOException;
 
 public class ProductListServlet extends HttpServlet {
     private ProductDao productDao = ArrayListProductDao.getInstance();
-    private CartService cartService = CartService.getInstance();
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
