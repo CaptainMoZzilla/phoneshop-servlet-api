628231f3ab59472673811e79dcc0215f3ff9b886
Anrix <derbylive145@gmail.com>
2018-09-08 21:13:05 +0300

    Added CartService

---
+++ src/main/java/com/es/phoneshop/model/cart/CartService.java
@@ -0,0 +1,57 @@
+package com.es.phoneshop.model.cart;
+
+import com.es.phoneshop.model.Product;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+public class CartService {
+    private static final CartService INSTANCE = new CartService();
+    private static final String CART_ATTRIBUTE_NAME = "cart";
+
+    private CartService() {
+    }
+
+    public static CartService getInstance() {
+        return INSTANCE;
+    }
+
+    public Cart getCart(HttpServletRequest request) {
+        HttpSession session = request.getSession();
+        Cart cart = (Cart) session.getAttribute(CART_ATTRIBUTE_NAME);
+
+        if (cart == null) {
+            synchronized (request.getSession()) {
+                cart = (Cart) session.getAttribute(CART_ATTRIBUTE_NAME);
+                if (cart == null) {
+                    cart = new Cart();
+                    session.setAttribute(CART_ATTRIBUTE_NAME, cart);
+                }
+            }
+        }
+        return cart;
+    }
+
+    public void addToCart(Cart cart, Product product, int quantity) {
+        CartItem cartItem = new CartItem(product, quantity);
+        int indexOfCartItem = cart.getCartItems().indexOf(cartItem);
+
+        if (quantity <= 0) {
+            throw new IllegalArgumentException("Error (<=0)");
+        }
+
+        if (indexOfCartItem == -1) {
+            if (quantity > product.getStock()) {
+                throw new IllegalArgumentException("It's too much");
+            }
+            cart.addToCart(cartItem);
+        } else {
+
+            if (quantity + cart.getCartItems().get(indexOfCartItem).getQuantity() > product.getStock()) {
+                throw new IllegalArgumentException("It's too much");
+            }
+            cartItem.setQuantity(quantity + cart.getCartItems().get(indexOfCartItem).getQuantity());
+            cart.setCartItem(indexOfCartItem, cartItem);
+        }
+    }
+}
